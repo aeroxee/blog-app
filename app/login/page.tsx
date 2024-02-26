b2224b434c58c37948c3e65581fd6af791c09153
Aeroxee <vipercode305@gmail.com>
2024-02-26 16:12:35 +0700

    ubah title dan favicon

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,15 @@
 import FormLogin from "@/components/FormLogin";
+import { Metadata, ResolvingMetadata } from "next";
+
+export async function generateMetadata(
+  _params: any,
+  _parent: ResolvingMetadata
+): Promise<Metadata> {
+  return {
+    title: "Login | Aeroxee Blog",
+    description: "Halaman login akun aeroxee blog",
+  };
+}
 
 export default function Login() {
   return <FormLogin />;
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,4 +1,15 @@
 import FormRegister from "@/components/FormRegister";
+import { Metadata, ResolvingMetadata } from "next";
+
+export async function generateMetadata(
+  _params: any,
+  _parent: ResolvingMetadata
+): Promise<Metadata> {
+  return {
+    title: "Register | Aeroxee Blog",
+    description: "Halaman pendaftaran akun aeroxee blog",
+  };
+}
 
 export default function Register() {
   return <FormRegister />;
